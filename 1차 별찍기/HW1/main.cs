a90ce8fe15ce4008b06517a728a7d2d0ddb3ea93
mintuchel <max3627@naver.com>
2024-04-02 04:26:27 +0900

    added boundaries

--- 1차 별찍기/HW1/main.cs
+++ 1차 별찍기/HW1/main.cs
@@ -1,33 +1,99 @@
 using System;
 using System.IO;
+using System.Runtime.ConstrainedExecution;
 
 namespace App
 {
+    public static class Default
+    {
+        public const int START_X = 25;
+        public const int START_Y = 10;
+        public const int WIDTH = 80; // 약 2배
+        public const int HEIGHT = 40;
+        
+        // 동적 커서
+        public static int startx;
+        public static int starty;
+        
+        public static void initCursorPos()
+        {
+            startx = START_X;
+            starty = START_Y;
+        }
+
+        public static void writeLine(string str)
+        {
+            Console.SetCursorPosition(startx, starty);
+            Console.Write(str);
+
+            // 개행처리
+            startx = START_X;
+            starty++;  
+        }
+
+        public static void write(string str)
+        {
+            Console.SetCursorPosition(startx, starty);
+            Console.Write(str);
+            startx++;
+        }
+
+        // 맨 처음 딱 한번 호출 후 더 이상 호출 안해도 되게
+        public static void drawBoard()
+        {
+            for (int i = 0; i < WIDTH; i++)
+            {
+                Console.Write("─");
+            }
+
+            for (int k = 1; k < HEIGHT; k++)
+            {
+                Console.SetCursorPosition(0, k);
+                Console.Write("│");
+            }
+
+            Console.SetCursorPosition(0, HEIGHT);
+            for (int i = 0; i < WIDTH; i++)
+            {
+                Console.Write("─");
+            }
+
+            for (int k = 1; k < HEIGHT; k++)
+            {
+                Console.SetCursorPosition(WIDTH, k);
+                Console.Write("│");
+            }
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write("┌");
+
+            Console.SetCursorPosition(WIDTH, 0);
+            Console.Write("┐");
+
+            Console.SetCursorPosition(0, HEIGHT);
+            Console.Write("└");
+
+            Console.SetCursorPosition(WIDTH, HEIGHT);
+            Console.Write("┘");
+        }
+    }
+
     class Result
     {
         int sel;
-        public Result(int sel)
+        public Result(int sel) 
         {
             this.sel = sel;
         }
 
-        ~Result()
-        {
-
-        }
-
         public int checkifValidNum()
         {
-
             // 줄 수 입력 : 바로 옆 자리 저장
+            // Default.startx Default.starty 적용 되어 있음
             int left = Console.CursorLeft;
-
-            int flag = 0;
 
             while (true)
             {
-                flag = Console.CursorLeft;
-
                 string dummy = "";
                 string str = Console.ReadLine();
 
@@ -46,7 +112,7 @@ namespace App
                     else
                     {
                         // (x,y)
-                        Console.SetCursorPosition(0, 2);
+                        Console.SetCursorPosition(Default.START_X, Default.START_Y+2);
                         Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("0보다 큰 정수 입력바람");
                         Console.ForegroundColor = ConsoleColor.White;
@@ -54,7 +120,7 @@ namespace App
                 }
                 else
                 {
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(Default.START_X, Default.START_Y + 2);
                     Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("유효한 값 입력바람    ");
                     Console.ForegroundColor = ConsoleColor.White;
@@ -65,51 +131,84 @@ namespace App
                 for (int i = 0; i < str.Length*2; i++) dummy += " ";
 
                 // 잘못입력된거 가리기
-                Console.SetCursorPosition(left, 0);
+                Console.SetCursorPosition(left, Default.starty);
                 Console.Write(dummy);
 
                 // 줄 수 입력 : 바로 옆으로 커서 배치
-                Console.SetCursorPosition(left, 0);
+                Console.SetCursorPosition(left, Default.starty);
             }
         }
 
         public void print1(int rows)
         {
+            // 커서 초기화
+            Default.initCursorPos();
+            Default.starty += 3;
+
             for (int i = 0; i < rows; i++)
             {
                 int x = 2 * i + 1;
-                for (int n = 0; n < rows - i - 1; n++) Console.Write(" ");
-                for (int n = 0; n < x; n++) Console.Write("*");
-                Console.WriteLine();
+                for (int n = 0; n < rows - i - 1; n++) Default.write(" "); // startx++;
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
             }
         }
 
         public void print2(int rows)
         {
+            Default.initCursorPos();
+            Default.starty += 3;
+
             for (int i = 0; i < rows; i++)
             {
                 int x = 2 * (rows - i - 1) + 1;
-                for (int n = 0; n < i; n++) Console.Write(" ");
-                for (int n = 0; n < x; n++) Console.Write("*");
-                Console.WriteLine();
+                for (int n = 0; n < i; n++) Default.write(" ");
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
             }
         }
 
         public void print3(int rows)
         {
-            print2(rows);
-            print1(rows);
+            Default.initCursorPos();
+            Default.starty += 3;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int x = 2 * (rows - i - 1) + 1;
+                for (int n = 0; n < i; n++) Default.write(" ");
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
+            }
+            for (int i = 0; i < rows; i++)
+            {
+                int x = 2 * i + 1;
+                for (int n = 0; n < rows - i - 1; n++) Default.write(" "); // startx++;
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
+            }
         }
 
         public void print4(int rows)
         {
-            print1(rows);
+            // 커서 초기화
+            Default.initCursorPos();
+            Default.starty += 3;
+
+            for (int i = 0; i < rows; i++)
+            {
+                int x = 2 * i + 1;
+                for (int n = 0; n < rows - i - 1; n++) Default.write(" "); // startx++;
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
+            }
+
             for (int i = 1; i < rows; i++)
             {
                 int x = 2 * (rows - i - 1) + 1;
-                for (int n = 0; n < i; n++) Console.Write(" ");
-                for (int n = 0; n < x; n++) Console.Write("*");
-                Console.WriteLine();
+                for (int n = 0; n < i; n++) Default.write(" ");
+                for (int n = 0; n < x; n++) Default.write("*");
+                Default.writeLine("");
             }
         }
 
@@ -136,24 +235,29 @@ namespace App
         */
         public void showResult()
         {
+            Default.drawBoard();
+            Default.initCursorPos();
+
             // 여기서 0 0 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write("줄 수 입력 : ");
+            Default.write("줄 수 입력 : ");
             Console.ForegroundColor = ConsoleColor.White;
 
             // 옳은 값 입력될때까지
             int rows = checkifValidNum();
 
             // 메세지 삭제 효과
-            Console.SetCursorPosition(0, 2);
-            Console.WriteLine("                      ");
+            Default.initCursorPos();
+            Default.starty += 2;
+            Default.write("                      ");
 
             if (sel == 0) print1(rows);
             else if (sel == 1) print2(rows);
             else if (sel == 2) print3(rows);
             else if (sel == 3) print4(rows);
 
-            Console.WriteLine("\nPRESS BACKSPACE TO GO BACK");
+            Default.starty++;
+            Default.write("PRESS BACKSPACE TO GO BACK");
             checkifBackSpace();
         }
     }
@@ -165,27 +269,28 @@ namespace App
 
         public void printSelectedLine(int sel)
         {
+            Default.initCursorPos();
             Console.ForegroundColor = ConsoleColor.Red;
             switch (sel)
             {
                 case 0:
-                    Console.SetCursorPosition(0, 0);
+                    Console.SetCursorPosition(Default.startx, Default.starty);
                     Console.Write("1. 가운데 정렬 별찍기");
                     break;
                 case 1:
-                    Console.SetCursorPosition(0, 1);
+                    Console.SetCursorPosition(Default.startx, Default.starty+1);
                     Console.Write("2. 1번의 반대로 찍기");
                     break;
                 case 2:
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(Default.startx, Default.starty+2);
                     Console.Write("3. 모래 시계");
                     break;
                 case 3:
-                    Console.SetCursorPosition(0, 3);
+                    Console.SetCursorPosition(Default.startx, Default.starty+3);
                     Console.Write("4. 다이아");
                     break;
                 case 4:
-                    Console.SetCursorPosition(0, 4);
+                    Console.SetCursorPosition(Default.startx, Default.starty+4);
                     Console.Write("5. 종료하기");
                     break;
             }
@@ -194,26 +299,27 @@ namespace App
 
         public void eraseSelectedLine(int before)
         {
+            Default.initCursorPos();
             switch (before)
             {
                 case 0:
-                    Console.SetCursorPosition(0, 0);
+                    Console.SetCursorPosition(Default.startx, Default.starty);
                     Console.Write("1. 가운데 정렬 별찍기");
                     break;
                 case 1:
-                    Console.SetCursorPosition(0, 1);
+                    Console.SetCursorPosition(Default.startx, Default.starty+1);
                     Console.Write("2. 1번의 반대로 찍기");
                     break;
                 case 2:
-                    Console.SetCursorPosition(0, 2);
+                    Console.SetCursorPosition(Default.startx, Default.starty+2);
                     Console.Write("3. 모래 시계");
                     break;
                 case 3:
-                    Console.SetCursorPosition(0, 3);
+                    Console.SetCursorPosition(Default.startx, Default.starty+3);
                     Console.Write("4. 다이아");
                     break;
                 case 4:
-                    Console.SetCursorPosition(0, 4);
+                    Console.SetCursorPosition(Default.startx, Default.starty+4);
                     Console.Write("5. 종료하기");
                     break;
             }
@@ -227,11 +333,13 @@ namespace App
 
         public void initial_render()
         {
-            Console.WriteLine("1. 가운데 정렬 별찍기");
-            Console.WriteLine("2. 1번의 반대로 찍기");
-            Console.WriteLine("3. 모래 시계");
-            Console.WriteLine("4. 다이아");
-            Console.WriteLine("5. 종료하기");
+            Default.drawBoard();
+            Default.initCursorPos();
+            Default.writeLine("1. 가운데 정렬 별찍기");
+            Default.writeLine("2. 1번의 반대로 찍기");
+            Default.writeLine("3. 모래 시계");
+            Default.writeLine("4. 다이아");
+            Default.writeLine("5. 종료하기");
         }
 
         public void showMenu()
@@ -290,12 +398,13 @@ namespace App
 
         public void render()
         {
-            Console.SetCursorPosition(0, 0);
+            // 커서 초기화
+            Default.initCursorPos();
             if (sel == 0) Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("1. 시작");
+            Default.writeLine("1. 시작");
             Console.ForegroundColor = ConsoleColor.White;
             if (sel == 1) Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("2. 종료");
+            Default.writeLine("2. 종료");
             Console.ForegroundColor = ConsoleColor.White;
         }
 
@@ -339,9 +448,11 @@ namespace App
         public static void Main()
         {
             Console.Title = "별찍기별찍기";
-            //Console.SetWindowSize(80, 40);
 
-            Console.CursorVisible = false;
+            // first and last drawBoard
+            Default.drawBoard();
+            Console.CursorVisible = true;
+
             Intro intro = new Intro();
             intro.startApp();
         }
