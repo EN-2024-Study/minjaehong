6a3afc57ccbc168e7baf77633abd210a1e18bb49
mintuchel <max3627@naver.com>
2024-04-10 21:16:02 +0900

    [ADD] userfrontcontroller and view

---
+++ 3차 라이브러리/Library/Controller/UserFrontController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class UserFrontController
+    {
+        private static UserFrontController instance;
+
+        UserController userController; // 추후에 호출해야되서 필요함
+        UserFrontView userFrontView; // FrontController에서 view 쓰려고 필요함
+        MemberModel memberModel; // LOGIN 확인 작업에 쓰임
+
+        private UserFrontController()
+        {
+            // 의존성 주입 느낌
+            this.userController = UserController.GetInstance();
+            this.userFrontView = UserFrontView.GetInstance();
+            this.memberModel = MemberModel.GetInstance();
+        }
+
+        public static UserFrontController GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UserFrontController();
+            }
+            return instance;
+        }
+
+        //===================== SINGELTON ========================//
+
+        public void run()
+        {
+            UserFrontMode userMode;
+
+            while (true)
+            {
+                // USER MODE 보여주기
+                userMode = userFrontView.UserFrontForm();
+
+                switch (userMode)
+                {
+                    case UserFrontMode.USER_LOGIN:
+
+                        List<string> loginInfo = userFrontView.UserLoginForm();
+
+                        // ID PW 이 둘 다 맞으면
+                        if (memberModel.CheckIfValidLogin(loginInfo))
+                        {
+                            string curUserID = loginInfo[0];
+                            userController.InitializeUserController(curUserID);
+                            userController.run();
+                        }
+                        break;
+
+                    case UserFrontMode.USER_CREATE_ACCOUNT:
+
+                        List<string> dataFromView = userFrontView.UserCreateAccountForm();
+                        MemberDTO newMember = new MemberDTO(dataFromView);
+                        memberModel.AddNewMember(newMember);
+
+                        break;
+                };
+            }
+        }
+    }
+}
---
+++ 3차 라이브러리/Library/View/UserFrontView.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class UserFrontView
+    {
+        private static UserFrontView instance;
+
+        private UserFrontView() { }
+
+        public static UserFrontView GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new UserFrontView();
+            }
+            return instance;
+        }
+
+        //===================== SINGELTON ========================//
+
+        public UserFrontMode UserFrontForm()
+        {
+            Console.Clear();
+
+            string[] userFrontMenuArr = { "LOGIN", "CREATE ACCOUNT" };
+
+            MyConsole.PrintHeader("[USER MODE]");
+            MyConsole.PrintAllMenu(userFrontMenuArr);
+
+            // UserFrontMode userFrontMode = 
+            return (UserFrontMode)MyConsole.GetUserSelection(userFrontMenuArr);
+
+            /*
+            // Redirect 느낌으로??
+            if (userFrontMode == UserFrontMode.USER_LOGIN)
+            {
+                UserLoginForm();
+            }
+            else
+            {
+                UserCreateAccountForm();                
+            }
+            */
+        }
+
+        // 1. LOGIN
+        public List<string> UserLoginForm()
+        {
+            Console.Clear();
+
+            string[] loginFormArr = { "ID", "PW" };
+
+            MyConsole.PrintHeader("[USER LOGIN]");
+            MyConsole.PrintAllMenu(loginFormArr);
+
+            List<string> loginInfo = MyConsole.GetUserInputs(loginFormArr.Length);
+            
+            return loginInfo;
+        }
+
+        // 2. CREATE ACCOUNT
+        // USER가 입력한 정보를 List<string> 형태로 다시 controller한테 넘겨줌
+        // controller는 이걸 바탕으로 MemberDTO를 만들어서 memberDB에 저장해주면 됨
+        public List<string> UserCreateAccountForm()
+        {
+            Console.Clear();
+
+            string[] createAccountArr = { "ID", "PW", "NAME", "AGE", "PHONENUM" };
+
+            MyConsole.PrintHeader("[USER CREATE ACCOUNT]");
+            MyConsole.PrintAllMenu(createAccountArr);
+            List<string> newMemberInfo = MyConsole.GetUserInputs(createAccountArr.Length);
+
+            return newMemberInfo;
+        }
+    }
+}
