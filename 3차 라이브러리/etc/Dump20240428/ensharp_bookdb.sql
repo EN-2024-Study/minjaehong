151d63e3bb81aa8e4adee1b09a8b34ed824e2d52
mintuchel <max3627@naver.com>
2024-04-28 06:24:55 +0900

    [ADD] dumpfolder

---
+++ 3차 라이브러리/etc/Dump20240428/ensharp_bookdb.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: ensharp
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookdb`
+--
+
+DROP TABLE IF EXISTS `bookdb`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookdb` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) DEFAULT NULL,
+  `author` varchar(15) DEFAULT NULL,
+  `publisher` varchar(15) DEFAULT NULL,
+  `price` varchar(15) DEFAULT NULL,
+  `instock` varchar(15) DEFAULT NULL,
+  `date` varchar(15) DEFAULT NULL,
+  `isbn` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookdb`
+--
+
+LOCK TABLES `bookdb` WRITE;
+/*!40000 ALTER TABLE `bookdb` DISABLE KEYS */;
+INSERT INTO `bookdb` VALUES (1,'book1','kim','companyA','1000','9','990317','111-11-1111'),(2,'book2','kim','companyB','2500','5','030811','222-22-2222'),(3,'book3','min','companyC','3000','2','011103','333-33-3333'),(4,'book4','min','companyB','500','7','971227','444-44-4444'),(5,'book5','park','companyB','4000','8','150710','555-55-5555');
+/*!40000 ALTER TABLE `bookdb` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-28  6:22:12
