fd95ec8194a0cf286cc876132c0339812cbcf69d
mintuchel <max3627@naver.com>
2024-05-02 13:16:35 +0900

    [FIX] resolved response display issue

--- 3차 라이브러리/Library/Service/NaverService.cs
+++ 3차 라이브러리/Library/Service/NaverService.cs
@@ -9,6 +9,14 @@ namespace Library
 {
     // 이 함수 자체를 static으로?
     // 어차피 함수 하나 밖에 안씀
+
+    // 동일한 이름 두 개 들어오면 터짐
+    // Dictionary라서 그럼 title 동일하면 이미 key 있어서 터짐
+    // List로 할까? 아니면 제목 괄호 그냥 유지?
+    // 이러면 문제가 제목 다쳐야함
+    // 그럼 제목 비슷하게만 치면 추가되게? 근데 그럼 뭐 부터 추가?? 
+    // 이거 생각해보기 
+    // 아니면 그냥 Dictionary말고 List 쓸까
     class NaverService
     {
         public static Dictionary<string,BookDTO> GetBooksByNaverAPI(RequestDTO requestDTO)
@@ -17,7 +25,7 @@ namespace Library
             Dictionary<string, BookDTO> naverBookList = new Dictionary<string, BookDTO>();
 
             string bookTitle = requestDTO.GetTitle();
-            string howMany = requestDTO.GetHowMany();
+            int howMany = int.Parse(requestDTO.GetHowMany());
 
             string sort = "sim";
             string query = string.Format("?query={0}&display={1}&sort={2}", bookTitle, howMany, sort);
@@ -33,25 +41,34 @@ namespace Library
 
             Stream stream = response.GetResponseStream();
             StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+            
             string text = reader.ReadToEnd();
 
-            // JSON-formatted string -> JsonObject instance로 매핑
-            // JObject = newtonjson에서 제공하는 Json 객체 담는 클래스
+            // JObject : newtonJson에서 제공하는 Json 객체"들" 담기 가능하게 해주는 클래스
+            // 추가정보들도 json data로 처리됨
+            // 추가 정보 + 검색 객체 배열을 모두 JObject로 받음
+
             JObject json = JObject.Parse(text);
 
-            for (int i = 0; i < int.Parse(howMany); i++)
+            // 추가 정보인 total 파싱해서 예외처리
+            int total = (int)json["total"];
+            if (total < howMany) howMany = total;
+
+            for (int i = 0; i < howMany; i++)
             {
                 string title = json["items"][i]["title"].ToString();
                 title = title.Replace("</b>", "").Replace("<b>", "");
                 
                 // 책 제목 길어지는거 방지
                 // 괄호 있는 부분부터 빼주고 trim으로 후행공백 모두 제거해주기
+                /*
                 if (title.Contains("("))
                 {
                     int idx = title.IndexOf("(");
                     title = title.Substring(0, idx);
                     title = title.Trim();
                 }
+                */
 
                 string author = json["items"][i]["author"].ToString();
                 string publisher = json["items"][i]["publisher"].ToString().Replace("<b>", "").Replace("</b>", "");
@@ -63,8 +80,8 @@ namespace Library
 
                 // 애초에 API로 받아올때 requested = true로 받아와서
                 // 애초에 받아올때부터 기존 bookDB에 있는 책들이랑 구분되게 하기
-                naverBookList[title] = new BookDTO(title, author, publisher, price, "1", date, isbn, false, true);
-                naverBookList.Add(title, new BookDTO(title, author, publisher, price, "1", date, isbn, false, true);
+                //naverBookList[title] = new BookDTO(title, author, publisher, price, "1", date, isbn, false, true);
+                naverBookList.Add(title, new BookDTO(title, author, publisher, price, "1", date, isbn, false, true));
             }
 
             response.Close();
