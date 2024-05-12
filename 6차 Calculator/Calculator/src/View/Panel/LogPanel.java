d90be6cab3da9d1069ae32f661bf3302554edaf3
mintuchel <max3627@naver.com>
2024-05-13 01:04:45 +0900

    [UPDATE] trashcan

--- 6차 Calculator/Calculator/src/View/Panel/LogPanel.java
+++ 6차 Calculator/Calculator/src/View/Panel/LogPanel.java
@@ -7,12 +7,12 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
 public class LogPanel extends JPanel{
-
-    // Label 들이 올라갈 labelPanel 따로 생성
-    JPanel labelPanel;
-    JScrollPane scrollPane;
+    
+    JPanel labelPanel; // Log label 들이 올라갈 labelPanel
+    JScrollPane scrollPane; // labelPanel 을 대상으로 만들 JScrollPane 객체
 
     JButton trashCanButton;
+    JPanel trashCanButtonPanel; // trashCanButton 을 붙일 flowlayout 형식의 panel
 
     public LogPanel(){
         createComponents();
@@ -20,30 +20,21 @@ public class LogPanel extends JPanel{
     }
 
     private void createComponents(){
-        labelPanel = new JPanel();
-        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
+        createScrollPane();
 
-        scrollPane = new JScrollPane(labelPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
-        trashCanButton = new JButton("TRASHCAN");
-        trashCanButton.setFont(new Font("Consolas", Font.BOLD, 14));
-        trashCanButton.setBackground(Color.ORANGE);
-        trashCanButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                labelPanel.removeAll();
-            }
-        });
+        createTrashCanButton();
+        createTrashCanPanel();
     }
 
     private void initializeLogPanel() {
 
+        createComponents();
+
         this.setLayout(new BorderLayout());
+        this.add(trashCanButtonPanel,BorderLayout.SOUTH); // 아래 쓰레기통 붙이기
 
-        scrollPane.setBorder(null);
-
-        this.add(trashCanButton,BorderLayout.SOUTH);
+        // label 들 붙일 labelPanel 을 들고 있는 scrollPane 을 LogPanel 에 붙이기
+        this.add(scrollPane, BorderLayout.CENTER);
 
         labelPanel.addContainerListener(new ContainerListener() {
             @Override
@@ -60,14 +51,40 @@ public class LogPanel extends JPanel{
                 }
             }
         });
+    }
 
-        // label 들 붙일 labelPanel 을 들고 있는 scrollPane 을 LogPanel 에 붙이기
-        this.add(scrollPane, BorderLayout.CENTER);
+    private void createScrollPane(){
+        labelPanel = new JPanel();
+        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
 
-        labelPanel.setVisible(true);
+        scrollPane = new JScrollPane(labelPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(null);
+    }
+
+    private void createTrashCanButton(){
+        ImageIcon trashCanIcon = new ImageIcon("src/Images/TrashcanIcon.png");
+
+        trashCanButton = new JButton(trashCanIcon);
+        trashCanButton.setSize(20,20);
+        trashCanButton.setBorderPainted(false);
+        trashCanButton.setContentAreaFilled(false);
         trashCanButton.setVisible(false);
 
-        setVisible(true);
+        trashCanButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                labelPanel.removeAll();
+            }
+        });
+    }
+
+    private void createTrashCanPanel(){
+        trashCanButtonPanel = new JPanel();
+        trashCanButtonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        trashCanButtonPanel.setBackground(Color.WHITE);
+
+        trashCanButtonPanel.add(trashCanButton);
     }
 
     // 로그 넣을때도 콤마 처리 해야함
